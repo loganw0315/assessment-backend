755d16547b64e74c82e311aea056a3cae6426f13
Logan Willis <loganw0315@gmail.com>
2021-11-06 18:38:14 -0500

    Backend assessment completed

---
+++ client/client.js
@@ -0,0 +1,120 @@
+let form = document.getElementById('lifeGoals')
+let goalInput = document.querySelector('input');
+
+//Get compliment
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+};
+//Get fortune
+document.getElementById("fortuneButton").onclick = function () {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+};
+//Get life goal suggestion
+document.getElementById("suggestGoalButton").onclick = function () {
+    axios.get("http://localhost:4000/api/lifegoal/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        });
+};
+
+
+
+
+let videoViewer = document.getElementById('video')
+
+
+//Change displayed video via drop down
+document.getElementById("videoList").onchange = function (event) {
+  event.preventDefault()
+  let videoSelector = document.querySelector("#videoList")
+  let selectedVideo = videoSelector.options[videoList.selectedIndex].text
+  let video = "";
+  if(selectedVideo === 'Never Give Up'){
+    video = 'https://www.youtube.com/embed/KxGRhd_iWuE'
+  } else if(selectedVideo === 'Just Do It'){
+    video = 'https://www.youtube.com/embed/ZXsQAXx_ao0'
+  } 
+
+  videoViewer.src = video
+    
+}
+
+
+let goals = document.querySelector('ul')
+//Fill goals list
+const getGoals = () => {
+  axios.get('http://localhost:4000/api/goals/')
+    .then(res => {
+      console.log(res.data);
+      goals.innerHTML = ''
+      
+      for(let i = 0; i < res.data.length; i++){
+        let newGoal = document.createElement('li');
+        let goal = document.createElement('span')
+        let deleteBtn = document.createElement('button');
+        newGoal.appendChild(goal);
+        newGoal.appendChild(deleteBtn);
+        goal.textContent = res.data[i].text
+        deleteBtn.textContent = "X";
+        newGoal.id = res.data[i].id
+        goals.appendChild(newGoal)
+        
+        deleteBtn.addEventListener('click', deleteGoal)
+        goal.addEventListener('click', editGoal)
+      }
+    })
+
+}
+
+//Add new goal
+const addGoal = (e) => {
+  e.preventDefault()
+  let addedGoal = {
+    text: goalInput.value
+  }
+
+  axios.post('http://localhost:4000/api/goals/', addedGoal)
+    .then(res => {
+      getGoals()
+    })
+  
+  goalInput.value = "";
+}
+//Delete a goal
+const deleteGoal = (event) => {
+    event.preventDefault()
+    let id = event.target.parentNode.id
+    axios.delete(`http://localhost:4000/api/goals/${id}`)
+      .then(res => {
+        getGoals()
+      })
+  
+}
+
+//Edit a goal
+const editGoal = (event) => {
+  event.preventDefault()
+  let changeGoal = {
+    id: event.target.parentNode.id,
+    text: goalInput.value
+  }
+  axios.put(`http://localhost:4000/api/goals/${changeGoal.id}`, changeGoal)
+    .then(res => {
+      getGoals()
+    })
+  goalInput.value = ""
+}
+
+
+form.addEventListener('submit', addGoal);
+getGoals()
+
---
+++ server/index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json()); // When we want to be able to accept JSON.
+
+
+
+//Compliments
+app.get("/api/compliment", (req, res) => {
+  const compliments = ["Gee, you're a smart cookie!",
+					 "Cool shirt!",
+					 "Your Javascript skills are stellar.",
+  ];
+
+  // choose random compliment
+  let randomIndex = Math.floor(Math.random() * compliments.length);
+  let randomCompliment = compliments[randomIndex];
+
+  res.status(200).send(randomCompliment);
+  
+});
+//Fortunes
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["A friend asks only for your time not your money.", "A person of words and not deeds is like a garden full of weeds.", "Adventure can be real happiness.", "Advice, when most needed, is least heeded.", "Believe in yourself and others will too."];
+
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
+});
+//Life goal suggestion
+app.get("/api/lifegoal", (req, res) => {
+  const goals = ["Drive your dream car", "Eat healthy", "Climb a mountain", "Learn to play an instrument", "Read 100 books"];
+
+  let randomIndex = Math.floor(Math.random() * goals.length);
+  let randomGoal = goals[randomIndex];
+
+  res.status(200).send(randomGoal);
+});
+
+//Life goal list
+let goals = [{'id': 0, 'text': 'hello' }];
+let goalId = 1;
+//Fill life goal list
+app.get(`/api/goals`, (req, res) => res.status(200).send(goals))
+//Add new goal
+app.post(`/api/goals`, (req, res) => {
+  let {text} = req.body
+  let addedGoal = {
+    id: goalId,
+    text
+  }
+  goals.push(addedGoal)
+  res.status(200).send(addedGoal)
+  goalId++
+})
+//Edit goal
+app.put(`/api/goals/:id`, (req, res) => {
+  let {id} = req.params
+  let {text} = req.body
+  let index = goals.findIndex(e => +e.id === +req.params.id)
+  goals[index].text = text
+  res.status(200).send(goals[+id])
+})
+//Remove goal
+app.delete(`/api/goals/:id`, (req, res) => {
+  let index = goals.findIndex(e => +e.id === +req.params.id)
+  deletedGoal = goals.splice(index, 1)
+  res.status(200).send(deletedGoal)
+})
+
+app.listen(4000, () => console.log("Server running on 4000"));
